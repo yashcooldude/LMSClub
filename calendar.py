854b3e84743e4d8361fdd1ae5ea4dca227cbb80d
yashcooldude <70448816+yashcooldude@users.noreply.github.com>
2020-10-03 13:11:15 -0700

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,14 @@
+def function():
+	pass
+	daytimesubject = "Day    Time      Subject "
+	print (daytimesubject)
+	day1 = """Monday  7:54-8:50  Period 0
+        9:10-10:15 Period 1
+       10:35-11:40 Period 2
+       12:10-01:15 Period 3"""
+	print (day1)
+	day2 = """Tuesday 9:10-10:15 Period 4
+       10:35-11:40 Period 5
+       12:10-01:15 Period 6"""
+	print(day2)
+function()
